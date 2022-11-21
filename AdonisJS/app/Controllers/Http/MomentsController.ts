0337d0bc5250c8c2f47db4831af395504f5f9c43
larissacard <larissak.cardosom@gmail.com>
2022-11-21 11:59:51 -0300

    fix: showing images

--- AdonisJS/app/Controllers/Http/MomentsController.ts
+++ AdonisJS/app/Controllers/Http/MomentsController.ts
@@ -8,35 +8,34 @@ import bodyParserConfig from 'Config/bodyparser';
 
 
 export default class MomentsController {
-
     private validationOptions = {
-        types: ['image'],
-        size: '2mb',
+      types: ['image'],
+      size: '2mb',
     }
-
-    public async store({request, response}: HttpContextContract) {
-        const body = request.body()
-
-        const moment = await Moment.create(body)
-        const image = request.file('image', this.validationOptions)
-
-        if(image) {
-            const imageName = `${uuidv4()}.${image.extname}`
-
-            await image.move(Application.tmpPath('uploads'), {
-                name: imageName
-            })
-
-            body.image = imageName
-        }
-
-
-        response.status(201)
-
-        return {
-            message: 'Momento criado com sucesso!',
-            date: moment,
-        }
+  
+    public async store({ request, response }: HttpContextContract) {
+      const body = request.body()
+  
+      const image = request.file('image', this.validationOptions)
+  
+      if (image) {
+        const imageName = `${uuidv4()}.${image!.extname}`
+  
+        await image.move(Application.tmpPath('uploads'), {
+          name: imageName,
+        })
+  
+        body.image = imageName
+      }
+  
+      const moment = await Moment.create(body)
+  
+      response.status(201)
+  
+      return {
+        message: 'Momento criado com sucesso!',
+        data: moment,
+      }
     }
 
     public async index(){
@@ -59,6 +58,8 @@ export default class MomentsController {
 
     public async destroy({params}: HttpContextContract){
         const moment = await Moment.findOrFail(params.id)
+
+        await moment.delete()
 
 
         return {
